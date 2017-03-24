e3c8522ad4723a1ac653eb74e40862dae44d5e51
slavashtat <slavaa@hotmail.com>
2017-03-24 23:03:17 +0100

    With Get /todos testinge

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,16 +4,38 @@ const request = require("supertest");
 const {app} = require("./../server");
 const {Todo} =  require("./../models/todo");
 
+/*
 beforeEach((done)=>
 {
  Todo.remove({}).then(()=> done());
 });
+*/
+
+const todos =
+[
+ {
+  text:"Play games"
+ },
+ {
+  text:"Smoke"
+ }
+];
+
+beforeEach((done)=>
+{
+ Todo.remove({}).then(()=> {
+  // return Todo.save(todos);  // does not work here
+  return Todo.insertMany(todos);
+ }).then(()=>done());
+});
+
+
 
 describe("Post /todos",() =>
 {
  it('should create a new todo',(done)=>
  {
-  var text = "Some 1 todo text";
+  var text = "Make a choice";
   request(app)
   .post('/todos')
   .send({text})
@@ -28,7 +50,7 @@ describe("Post /todos",() =>
    {
     return done(err);
    }
-   Todo.find().then((todos)=>
+   Todo.find({text}).then((todos)=>
    {
     expect(todos.length).toBe(1);
     expect(todos[0].text).toBe(text);
@@ -53,7 +75,7 @@ describe("Post /todos",() =>
    }
    Todo.find().then((todos)=>
    {
-    expect(todos.length).toBe(0);
+    expect(todos.length).toBe(2);
     //expect(todos[0].text).toBe(text);
     done();
    }).catch((e)=> {
@@ -64,3 +86,40 @@ describe("Post /todos",() =>
 }); //describe
 
 
+describe("Get /todos",()=>
+{
+ it("It should get all todos",(done)=>
+ {
+  request(app)
+  .get('/todos')
+  .expect(200)
+  .expect((res)=>{
+   expect(res.body.todos.length).toBe(2);
+  })
+  .end(done) // as we are not doing anything async, we don't do
+             // end((err,res) => {}
+ });
+});
+
+/*
+describe("Get /todos",()=>
+{
+ it("It should get all todos",(done)=>
+ {
+  request(app)
+  .get('/todos')
+  .expect(200)
+  .expect((res)=>{
+   expect(res.body.todos.length).toBe(2);
+  })
+  .end((err,res) =>
+  {
+   if(err)
+   {
+    return done(err);
+   }
+   done();
+  });
+ });
+}); //describe
+*/
