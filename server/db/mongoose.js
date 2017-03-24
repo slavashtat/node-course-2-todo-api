79a95947f4f52c3f9076949a28c77f1b5eb292ef
slavashtat <slavaa@hotmail.com>
2017-03-24 16:38:16 +0100

    Test POST /todos

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise; // As mongoose deoes not know which promiss library to use
 mongoose.connect("mongodb://localhost:27017/TodoApp");
 
 
--- server/server.js
+++ server/server.js
@@ -7,24 +7,30 @@ var {User} =require ("./models/user");
 
 var app = express();
 
+// Returns middleware that only parses json. This parser accepts any Unicode
+// encoding of the body and supports automatic inflation of gzip and deflate
+// encodings.
 app.use(bodyParser.json());
 
-app.post("/todos",(req,res) =>
-{
- var todo = new Todo({text: req.body.text});
 
- todo.save().then((doc) =>
- {
+app.post('/todos', (req, res) => {
+ var todo = new Todo({
+  text: req.body.text
+ });
+
+ todo.save().then((doc) => {
   res.send(doc);
- }, (e) =>
- {
+ }, (e) => {
   res.status(400).send(e);
  });
 });
 
 
-
-
 app.listen(3000, () =>{
  console.log("Started on port 3000");
 });
+
+module.exports = {app};
+/////////////
+
+
