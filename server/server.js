57cdc947d8143b13ad9cc1bae269c1a3381d5928
slavashtat <slavaa@hotmail.com>
2017-03-25 11:28:23 +0100

    with GET/todos/id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ var bodyParser = require("body-parser");
 var {mongoose} =  require ("./db/mongoose");
 var {Todo} =require ("./models/todo");
 var {User} =require ("./models/user");
+const {ObjectID} = require('mongodb');
+
 
 var app = express();
 
@@ -36,11 +38,37 @@ app.get('/todos', (req, res) => {
  {
   res.status(400).send(e);
  });
-
-
-
-
 });
+
+
+app.get("/todos/:id",(req,res)=>
+{
+ var id = req.params.id;
+ if (!ObjectID.isValid(id))
+ {
+  var body = id + " invalid object id";
+  //return res.status(404).send(body);
+  return res.status(404).send();
+ }
+
+ Todo.findById(id).then((todo)=>
+ {
+  if(!todo)
+  {
+   return res.status(404).send();
+  }
+
+  res.send({todo});
+ }).catch( (e)=>
+ {
+  res.status(400).send();
+ });
+});
+
+
+
+
+
 
 
 
